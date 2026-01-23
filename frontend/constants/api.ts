7de523a9c27318207a37be449d24e1b3d2c9587f
Shiva Nandan V <shiva.nandan@juspay.in>
2026-01-24 01:01:16 +0530

    update api_base_url to work with remote hosted web app

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 
 // API base URL - uses localhost for web, machine IP for native devices
 export const API_BASE_URL = Platform.OS === 'web' 
-  ? 'http://localhost:3000' 
+  ? window.location.protocol + '//' + window.location.hostname + ':3000' 
   : 'http://namma-chennai.app.seekshiva.in:3000';
 
 export const ENDPOINTS = {
